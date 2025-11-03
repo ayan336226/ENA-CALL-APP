d10dc08b9312d1a510e4d9862acd2f0f54926590
ayan336226 <muhammadayan336226@gmail.com>
2025-11-02 23:13:50 -0800

    Fixed: added root route for index.html

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const HOST = '0.0.0.0';
 server.listen(PORT, HOST, () => {
   console.log(`Server running on http://${HOST}:${PORT}`);
