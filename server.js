677a49c7ae7089124f3ce06342c106d135ad164d
ayan336226 <muhammadayan336226@gmail.com>
2025-11-02 20:21:36 -0800

    Updated server.js for Replit port fix

--- server.js
+++ server.js
@@ -74,4 +74,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => console.log("🚀 Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`);
+});
+
